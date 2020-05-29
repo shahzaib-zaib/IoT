3f41a742caa491a363aeafc5326f23d81eda7ed3
Shahzaib <shahzaibux@gmail.com>
2020-05-29 19:12:05 +0500

    Created Numbersys

---
+++ numbersys/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let DecimalNumber = 100;
+    let HexaDecimalNumber = 0xff;
+    let OcatalNumber = 0o77;
+    let BinaryNumbersys = b'10101010;
+    let c = b'8;
+
+
+    println!("{}", DecimalNumber);
+    println!("{}", HexaDecimalNumber);
+    println!("{}", OcatalNumber);
+    println!("{}", BinaryNumbersys);
+    println!("{}", c);
+
+
+}
