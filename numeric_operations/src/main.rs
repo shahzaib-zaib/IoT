13057c930933a87ae5c3ca018940a2fb76d9b5c9
Shahzaib <shahzaibux@gmail.com>
2020-05-29 19:35:36 +0500

    Created Numeric Operations

---
+++ numeric_operations/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+
+    let x = 9;
+    let y = 5;
+
+    let sum = x + y;
+    let minus = x - y;
+    let div = x / y;
+    let mul = x * y;
+    let rem = x % y;
+
+    println!("{}", sum);
+    println!("{}", minus);
+    println!("{}", div);
+    println!("{}", mul);
+    println!("{}", rem);
+
+}
