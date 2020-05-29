42eaa714887cc26a597b93edf9685ec8590ca4df
Shahzaib <shahzaibux@gmail.com>
2020-05-29 19:55:35 +0500

    Created Arrays

---
+++ Arrays/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let lottary_number: [u32,5] = [1,22,45,56,78];
+
+    println!("{:#?}", lottary_number);
+
+        let rupee_five = [5;10];
+        println!("{:#?}", rupee_five);
+
+        let days = ['M', 'T', 'W'];
+        println!("{:#?}", days);
+
+
+}
