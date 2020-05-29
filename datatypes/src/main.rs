ba63c0562ed8df4c025d232a1857f8c2156412d8
Shahzaib <shahzaibux@gmail.com>
2020-05-29 19:04:58 +0500

    Created DataTypes

---
+++ datatypes/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    //scalar data types
+    let age = 25;
+    let percentage = 80.3;
+    let grade = 'A';
+    let passed = true;
+
+
+    let secret_number : i32 = -80;
+    println!("{}", secret_number);
+}
