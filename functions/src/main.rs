0ceba727864fe93aa6958fbf1bc3851a0ee04611
Shahzaib <shahzaibux@gmail.com>
2020-05-29 21:31:12 +0500

    Created Functions

---
+++ functions/src/main.rs
@@ -0,0 +1,33 @@
+// fn main() {
+//     println!("BAKER SHARING RECIPE WITH FIRST PERSON");
+//         paper();
+
+//     println!("BAKER SHARING RECIPE WITH SECOND PERSON");
+//         paper();
+
+//     println!("BAKER SHARING RECIPE WITH THIRD PERSON");
+//         paper();
+
+//     fn paper(){
+//         println!("1. Add Milk");
+//         println!("2. Add Butter");
+//         println!("3. Add Eggs");
+//         println!("4. Add Suger");
+//         println!("5. Stir it");
+//         println!("6. Heat on gentle flase");
+//     }
+// }
+
+
+fn main(){
+    square(2,9);
+}
+
+fn square(x: u32, y: u32){
+    let result = x* x;
+    let result_1 y* y;
+
+    println!("square of no {} is {}", x,result);
+    println!("square of no {} is {}", x,result_1);
+
+}
