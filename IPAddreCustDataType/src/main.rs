0807b89462a2d23a38666adb8181d3e2ebfcdc57
Shahzaib <shahzaibux@gmail.com>
2020-07-06 14:42:10 +0500

    Updated Project

--- IPAddreCustDataType/src/main.rs
+++ IPAddreCustDataType/src/main.rs
@@ -1,9 +1,29 @@
 fn main() {
-    let four = IPAddrKind::V4;
-    let six = IPAddrKind::v6;
+    let ip_address1 = IPAddress{
+        Kind: IPAddrKind::V4,
+        address: String::from("127.0.0.1")
+    };
+    
+    println!("{:#?}",ip_address1);
+
+    let ip_address2 = IPAddress{
+        kind: IPAddrKind::V6,
+        address: String::from("::1")
+    };
+
+    println!("{:#?}",ip_address2);
 }
 
+
+
+#[derive(Debug)]
 enum IPAddrKind{
     V4,
     V6
+}
+
+#[derive(Debug)]
+struct IPAddress{
+    kind: IPAddrKind,
+    address: String
 }
